b6cab25aef3001ec447c177126b2ddfbdb2db826
itggot-ludvig-pennert <38528579+itggot-ludvig-pennert@users.noreply.github.com>
2019-03-17 23:03:53 +0100

    Updates

--- app.rb
+++ app.rb
@@ -5,7 +5,15 @@ require 'bcrypt'
 
 enable :sessions
 
+db = SQLite3::Database.new("db/database.db")
+db.results_as_hash = true
 
+post('/newpost') do
+    user = session[:username]
+    content = params["content"]
+    db.execute("INSERT INTO posts (content, username) VALUES ('#{content}', '#{user}')")
+    redirect('/welcome')
+end
 
 get('/') do
     slim(:index)
@@ -28,32 +36,30 @@ get('/welcome/:profile') do
     slim(:profile)
 end
 
+post('/deletepost') do
+    db.execute("DELETE FROM posts WHERE postid = #{params[:postid]}")
+    redirect('/welcome')
+end
+
 post('/login') do
     # om id:t för användarnamn och password är samma, redirect till startsidan
     # om det inte är samma redirect till access denied
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
     usercred = db.execute("SELECT username, password FROM users WHERE username =?",params["username"])
     if usercred == []
         redirect('/denied')
     end
-
     if checkpassword(usercred[0][1],params["password"],usercred[0][0]) == true 
         redirect("/welcome")
     else
         redirect("/denied")
     end
-
-    
-
 end
+
 get('/denied') do
     slim(:denied)
 end
 
 post('/register') do
-    db = SQLite3::Database.new("db/database.db") 
-    hashedpassword = BCrypt::Password.create(params["password"])
     begin
         db.execute("INSERT INTO users(username,password) VALUES (?,?)",params["username"],hashedpassword)
     rescue SQLite3::ConstraintException => exception
